bb69f14e0fd3839afb0cb50b86f823b4d572d913
Lester Royales <lesterroyals041203@gmail.com>
2025-09-10 13:21:04 +0800

    Interactive Workstation

--- src/components/Workstations/Workstations.tsx
+++ src/components/Workstations/Workstations.tsx
@@ -2,7 +2,7 @@ import './worksta.css';
 function Workstations() {
   return (
     <div>
-      <h1 className="text-2xl font-bold bg-purple-500">Workstations Page</h1>;
+      <h1 className="text-2xl font-bold bg-purple-500">interactive Workstations Page</h1>;
     </div>
   )
 }
